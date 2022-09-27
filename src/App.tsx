27661deced00ecf1706796a09b32366c6a0d2000
glav-git <alexeyholostenko96@gmail.com>
2022-09-28 00:13:12 +0300

    add Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-
+import Card from "./components/Card"
 
 const App = () => {
   return (
     <div>
-      Working!
+      <Card width="200px" height="200px"/>
     </div>
   )
 }
