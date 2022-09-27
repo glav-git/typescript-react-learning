0dd11f0d0890af951cfcf077b70780d4560f573f
glav-git <alexeyholostenko96@gmail.com>
2022-09-28 00:49:42 +0300

    make Card component functional (implicit children props not working)

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {FC} from 'react';
 
 interface CardProps {
     width?: string;
     height?: string;
-    children?: React.ReactChild | React.ReactNode
+    children?: React.ReactNode
 }
 
-const Card = ({width, height, children}: CardProps) => {
+const Card: FC<CardProps> = 
+    ({
+        width, 
+        height, 
+        children
+    }) => {
     return (
         <div style={{width, height, border: '1px solid gray'}}>
             {children}
