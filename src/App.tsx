21a6bebbbd36cff938f1618f318510088e10ff86
glav-git <alexeyholostenko96@gmail.com>
2022-09-28 00:18:06 +0300

    add children support for Card component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Card from "./components/Card"
 const App = () => {
   return (
     <div>
-      <Card width="200px" height="200px"/>
+      <Card width="200px" height="200px">
+        <button>button</button>
+      </Card>
     </div>
   )
 }
