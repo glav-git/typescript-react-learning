f5f0cfa4f65c75febb6b201531399052680de227
glav-git <alexeyholostenko96@gmail.com>
2022-09-28 00:59:55 +0300

    add variant props to Card component that enum typed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Card from "./components/Card"
+import Card, { CardVariant } from "./components/Card"
 
 const App = () => {
   return (
     <div>
-      <Card width="200px" height="200px">
+      <Card variant={CardVariant.primary} width="200px" height="200px">
         <button>button</button>
       </Card>
     </div>
