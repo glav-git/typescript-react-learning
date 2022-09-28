99555179638ded3fc9a054ce521534ab0f19e3d6
glav-git <alexeyholostenko96@gmail.com>
2022-09-28 12:27:48 +0300

    render todo list using generic List

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import Card, { CardVariant } from "./components/Card"
 import List from "./components/List";
+import TodoItem from "./components/TodoItem";
 import UserItem from "./components/UserItem"
 import { ITodo, IUser } from "./types/types"
 
@@ -40,6 +41,7 @@ const App = () => {
         <button>button</button>
       </Card>
       <List items={users} renderItem={ (user: IUser) => <UserItem user={user} key={user.id}/>} />
+      <List items={todos} renderItem={ (todo: ITodo) => <TodoItem todo={todo} key={todo.id}/>} />
     </div>
   )
 }
