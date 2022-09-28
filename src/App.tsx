11f2f451688ed18d48fe1c4a80cddff20e485e02
glav-git <alexeyholostenko96@gmail.com>
2022-09-28 16:27:42 +0300

    make routes for all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import Card, { CardVariant } from "./components/Card"
 import EventsExample from "./components/EventsExample";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserPage from "./components/UserPage";
 import TodosPage from "./components/TodosPage";
 
 const App = () => {
 
   return (
-    <div>
-      <EventsExample />
-      <Card onClick={(num) => console.log('Andrey, andrushka, pososi moyu pisushku', num)} variant={CardVariant.primary} width="200px" height="200px">
-        <button>button</button>
-      </Card>
-      <UserPage />
-      <TodosPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/drag_events'} element={<EventsExample />}/>
+        <Route path={'/users'} element={<UserPage />}/>
+        <Route path={'/todos'} element={<TodosPage />}/>
+          
+        <Route path={'/card'} element={
+          <Card onClick={(num) => console.log('Andrey, andrushka, pososi moyu pisushku', num)} variant={CardVariant.primary} width="200px" height="200px">
+            <button>button</button>
+          </Card>
+        }/>
+        <Route path={'/sandbox'} element={
+          <div>
+            <Card onClick={(num) => console.log('Andrey, andrushka, pososi moyu pisushku', num)} variant={CardVariant.primary} width="200px" height="200px">
+              <button>button</button>
+            </Card>
+            <EventsExample />
+            <UserPage />
+            <TodosPage />
+          </div>
+        }/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
