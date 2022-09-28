f47679b7346e57e235826e87fb91d8d1bfe53ea2
glav-git <alexeyholostenko96@gmail.com>
2022-09-28 10:33:28 +0300

    create typed state for list of users

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Card, { CardVariant } from "./components/Card"
+import UserList from "./components/UserList"
+import { IUser } from "./types/types"
 
 const App = () => {
+  const [users, setUsers] = useState<IUser[]>([])
+
   return (
     <div>
       <Card onClick={(num) => console.log('Andrey, andrushka, pososi moyu pisushku', num)} variant={CardVariant.primary} width="200px" height="200px">
         <button>button</button>
       </Card>
+      <UserList users={users}/>
     </div>
   )
 }
