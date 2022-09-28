642dd36b086739ecb0fa7ac5267f5069d26b20f2
glav-git <alexeyholostenko96@gmail.com>
2022-09-28 09:34:09 +0300

    add function (no parameters and void return type) to props interface

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Card, { CardVariant } from "./components/Card"
 const App = () => {
   return (
     <div>
-      <Card variant={CardVariant.primary} width="200px" height="200px">
+      <Card onClick={() => console.log('Andrey, andrushka, pososi moyu pisushku')} variant={CardVariant.primary} width="200px" height="200px">
         <button>button</button>
       </Card>
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,10 +6,11 @@ export enum CardVariant {
 }
 
 interface CardProps {
-    width?: string;
+    width?: string; // ? symbol means that interface parameter optional
     height?: string;
     children?: React.ReactNode;
     variant: CardVariant;
+    onClick?: () => void; //  empty brackets describe function with no parameters and void means function return type
 }
 
 const Card: FC<CardProps> = 
@@ -17,13 +18,16 @@ const Card: FC<CardProps> =
         width, 
         height, 
         variant,
+        onClick,
         children,
     }) => {
     return (
         <div style={{width, height, 
             border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
             background: variant === CardVariant.primary ? 'lightgray' : ''
-            }}>
+            }}
+             onClick={onClick}
+        >
             {children}
         </div>
     );
