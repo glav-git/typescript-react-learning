977600a11d2e71cfe9e5af82439115ac80bb2762
glav-git <alexeyholostenko96@gmail.com>
2022-09-28 19:48:29 +0300

    make navigation bar for all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Card, { CardVariant } from "./components/Card"
 import EventsExample from "./components/EventsExample";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import UserPage from "./components/UserPage";
 import TodosPage from "./components/TodosPage";
 
@@ -8,6 +8,13 @@ const App = () => {
 
   return (
     <BrowserRouter>
+      <div style={{border: '1px solid pink', display: 'flex', width: '300px', justifyContent: 'space-around', flexWrap: 'wrap', margin: "5px 0", padding: "2px 0"}}>
+        <Link to={'/users'}>Users</Link>
+        <Link to={'/todos'}>Todos</Link>
+        <Link to={'/card'}>Card</Link>
+        <Link to={'/drag_events'}>DragEvents</Link>
+        <Link to={'/sandbox'}>Sandbox</Link>
+      </div>
       <Routes>
         <Route path={'/drag_events'} element={<EventsExample />}/>
         <Route path={'/users'} element={<UserPage />}/>
