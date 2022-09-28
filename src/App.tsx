6d04972346197146dbd1938445d729fd510196e8
glav-git <alexeyholostenko96@gmail.com>
2022-09-28 12:26:34 +0300

    fetching todo list on page loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,34 @@ import { useState, useEffect } from "react";
 import Card, { CardVariant } from "./components/Card"
 import List from "./components/List";
 import UserItem from "./components/UserItem"
-import { IUser } from "./types/types"
+import { ITodo, IUser } from "./types/types"
 
 const App = () => {
   const [users, setUsers] = useState<IUser[]>([])
+  const [todos, setTodos] = useState<ITodo[]>([])
 
   useEffect( () => {
     fetchUsers()
+    fetchTodos()
   }, [])
 
   async function fetchUsers() {
     try {
       // generic saing then in response we waiting for Users array
-      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users') 
-      setUsers(response.data)
+      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+      setUsers(response.data);
     } catch (e) {
-      alert(e)
+      alert(e);
+    }
+  }
+
+  async function fetchTodos() {
+    try {
+      // generic saing then in response we waiting for Users array
+      const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10');
+      setTodos(response.data);
+    } catch (e) {
+      alert(e);
     }
   }
 
