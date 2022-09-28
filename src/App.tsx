90df3aded094481d57a6065f5b56435f4e49b24e
glav-git <alexeyholostenko96@gmail.com>
2022-09-28 13:39:36 +0300

    add onDrag events and render example in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Card, { CardVariant } from "./components/Card"
+import EventsExample from "./components/EventsExample";
 import List from "./components/List";
 import TodoItem from "./components/TodoItem";
 import UserItem from "./components/UserItem"
@@ -37,6 +38,7 @@ const App = () => {
 
   return (
     <div>
+      <EventsExample />
       <Card onClick={(num) => console.log('Andrey, andrushka, pososi moyu pisushku', num)} variant={CardVariant.primary} width="200px" height="200px">
         <button>button</button>
       </Card>
--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,20 +1,48 @@
 import React, { FC, useState } from "react";
 
 const EventsExample: FC = () => {
-    const [inputValue, setInputValue] = useState<string>('')
+    const [inputValue, setInputValue] = useState<string>('');
+    const [isDrag, setIsDrag] = useState<boolean>(false);
 
     const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue(e.target.value)
+        setInputValue(e.target.value);
     }
 
     const buttonClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-        console.log(inputValue)        
+        console.log(inputValue);
+    }
+
+    const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        console.log('Fisting started');
+    }
+
+    const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(false);
+        console.log('Fisting droped')
+    }
+
+    const dragWithPreventHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(true);
+    }
+
+    const leaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(false);
     }
 
     return (
         <div>
             <input value={inputValue} onChange={inputChangeHandler} type="text" />
             <button onClick={buttonClickHandler}>button with onClick</button>
+            <div onDrag={dragHandler} draggable style={{width: 200, height: 200, background: 'red'}}></div>
+            <div 
+                onDrop={dropHandler} 
+                onDragLeave={leaveHandler}
+                onDragOver={dragWithPreventHandler}
+                style={{width: 200, height: 200, background: isDrag ? 'blue' : 'red', marginTop: 15}}>
+            </div>
         </div>
     );
 };
