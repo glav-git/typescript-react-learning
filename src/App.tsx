ec6f3693e0cf0bc293581e7944bc72d488d52f91
glav-git <alexeyholostenko96@gmail.com>
2022-09-28 12:08:17 +0300

    replace UserList component with universal generic List

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Card, { CardVariant } from "./components/Card"
-import UserList from "./components/UserList"
+import List from "./components/List";
+import UserItem from "./components/UserItem"
 import { IUser } from "./types/types"
 
 const App = () => {
@@ -26,7 +27,7 @@ const App = () => {
       <Card onClick={(num) => console.log('Andrey, andrushka, pososi moyu pisushku', num)} variant={CardVariant.primary} width="200px" height="200px">
         <button>button</button>
       </Card>
-      <UserList users={users}/>
+      <List items={users} renderItem={ (user: IUser) => <UserItem user={user} key={user.id}/>} />
     </div>
   )
 }
