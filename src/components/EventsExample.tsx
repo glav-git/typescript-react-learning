e765adec9be54199cae9225792ebd804c9827a77
glav-git <alexeyholostenko96@gmail.com>
2022-09-28 14:24:26 +0300

    add not controlled input using typed useRef

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,15 +1,18 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useRef } from "react";
 
 const EventsExample: FC = () => {
     const [inputValue, setInputValue] = useState<string>('');
     const [isDrag, setIsDrag] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInputValue(e.target.value);
     }
 
     const buttonClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-        console.log(inputValue);
+        console.log("Управляемый: ", inputValue);
+        // ? means optional chaining, needed cause current can be null
+        console.log("Неуправляемый: ", inputRef.current?.value);
     }
 
     const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
@@ -19,7 +22,7 @@ const EventsExample: FC = () => {
     const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
         e.preventDefault();
         setIsDrag(false);
-        console.log('Fisting droped')
+        console.log('Fisting droped');
     }
 
     const dragWithPreventHandler = (e: React.DragEvent<HTMLDivElement>) => {
@@ -34,7 +37,8 @@ const EventsExample: FC = () => {
 
     return (
         <div>
-            <input value={inputValue} onChange={inputChangeHandler} type="text" />
+            <input value={inputValue} onChange={inputChangeHandler} type="text" placeholder="Управляемый"/>
+            <input ref={inputRef} type="text" placeholder="Неуправляемый"/>
             <button onClick={buttonClickHandler}>button with onClick</button>
             <div onDrag={dragHandler} draggable style={{width: 200, height: 200, background: 'red'}}></div>
             <div 
